f2dc5f65fcdd7810a3eaf861d416a48db0c938fd
akshaykrishna129 <akshaykrish129@gmail.com>
2021-11-11 00:26:22 -0600

    added

--- controllers/land.js
+++ controllers/land.js
@@ -31,9 +31,26 @@ exports.land_detail = function(req, res) {
 }; 
  
 // Handle land create on POST. 
-exports.land_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: land create POST'); 
+exports.land_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new land(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.land_type = req.body.land_type; 
+    document.cost = req.body.cost; 
+    document.size = req.body.size; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
+ 
  
 // Handle land delete form on DELETE. 
 exports.land_delete = function(req, res) { 
