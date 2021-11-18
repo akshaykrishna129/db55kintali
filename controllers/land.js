04c67ef07ff2ae144c69a574186bb1e31904f02c
akshaykrishna129 <akshaykrish129@gmail.com>
2021-11-17 18:49:31 -0600

    added

--- controllers/land.js
+++ controllers/land.js
@@ -92,3 +92,16 @@ ${JSON.stringify(req.body)}`)
 failed`); 
     } 
 }; 
+ // Handle a show one view with id specified by query 
+ exports.land_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await land.findById( req.query.id) 
+        res.render('landdetail',  
+{ title: 'land Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/land.js
+++ routes/land.js
@@ -10,5 +10,7 @@ module.exports = router;
 router.get("/", function (req, res, next) {
   res.render("land", { title: "Search Results Land" });
 });
+/* GET detail costume page */ 
+router.get('/detail', land_controlers.land_view_one_Page); 
 
 module.exports = router;
