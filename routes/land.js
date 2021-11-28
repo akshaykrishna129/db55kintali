058e02e4b1589dc19c1ac1485ad4e42613b4dfd2
akshaykrishna129 <akshaykrish129@gmail.com>
2021-11-28 15:17:49 -0600

    added

--- routes/land.js
+++ routes/land.js
@@ -1,6 +1,16 @@
 var express = require("express");
 const land_controlers= require('../controllers/land'); 
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET land */ 
 router.get('/', land_controlers.land_view_all_Page ); 
@@ -15,7 +25,7 @@ router.get('/detail', land_controlers.land_view_one_Page);
 /* GET create costume page */ 
 router.get('/create', land_controlers.land_create_Page); 
 /* GET create update page */ 
-router.get('/update', land_controlers.land_update_Page); 
+router.get('/update', secured, land_controlers.land_update_Page); 
 /* GET create land page */ 
 router.get('/delete', land_controlers.land_delete_Page); 
 
